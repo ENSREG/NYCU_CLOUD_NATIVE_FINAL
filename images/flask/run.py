752b6df768eb6d844e5816ee13cc20eb0f358157
Ian Chen <ychen.desl@gmail.com>
2022-05-30 16:43:08 +0800

    fix the lint error

--- images/flask/run.py
+++ images/flask/run.py
@@ -1,7 +1,13 @@
 from flask import Flask
-app=Flask(__name__)
+app = Flask(__name__)
+
+
 @app.route('/')
+
+
 def hello():
     return 'hello !'
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080)
+    app.run(host = '0.0.0.0', port = 8080)
