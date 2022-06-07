9b1164b2910f240a91c6257f87c4c85f8a79362e
Ian Chen <ychen.desl@gmail.com>
2022-06-07 16:58:06 +0800

    update the cors policy

--- images/flask/run.py
+++ images/flask/run.py
@@ -5,7 +5,7 @@ from flask import Flask, request, Response
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/.*": {"origins": "*"}})
 
 PredFilePath = './Pred.csv'  # csv file Path
 GroundTruthFilePath = './GroundTruth.csv'
